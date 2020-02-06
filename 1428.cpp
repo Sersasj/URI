f6ec933e24f74a6ad0b63cd9190af2849b853925
Sersasj <32242350+Sersasj@users.noreply.github.com>
2020-02-06 00:17:40 -0300

    Create 1428.cpp

---
+++ 1428.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+using namespace std;
+
+int main(){
+float tempo,velocidade;
+  int x,i,a,b;
+  cin>>i;
+  for(x=0;x<i;x++){
+    cin>>a>>b;
+  cout << (a/3)*(b/3)<<endl;
+  
+  }
+ 
+};
