05fab1a4921b5891cfeb1eb914b5745055439815
Sersasj <32242350+Sersasj@users.noreply.github.com>
2020-09-30 19:11:37 -0300

    Create 1097.cpp

---
+++ 1097.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <iomanip>
+#include <vector>
+
+using namespace std;
+
+
+int main() {
+  int n, i=1,j=7;
+  
+  
+  while(i<=9){
+    cout << "I="<<i<<" J="<<j--<< endl;
+    cout << "I="<<i<<" J="<<j--<< endl;
+    cout << "I="<<i<<" J="<<j--<< endl;
+    i = i+2;
+    j = j+5;
+    
+
+  }
+}
